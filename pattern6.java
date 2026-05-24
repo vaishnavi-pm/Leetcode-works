578dbe79a364276a56a84eef7fcef965adb420d8
VAISHNAVI PM <vaishpm15@gmail.com>
2026-05-24 17:43:45 +0000

    Pattern6

---
+++ pattern6.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void pattern4(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
