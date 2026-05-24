a56726c470c6268d8306655b3c36597bd77a8bd8
VAISHNAVI PM <vaishpm15@gmail.com>
2026-05-24 18:19:41 +0000

    pattern8

---
+++ pattern8.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void pattern5(int n) {
+        for (int i = n; i <= 1; i--) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
